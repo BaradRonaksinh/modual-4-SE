e1dfb5a6e624d7d4dd0d389a3af2549e363c8c4d
BaradRonaksinh <baradronaksinh21303@gmail.com>
2024-02-27 15:20:45 +0530

    solve error in one file

--- Inheritance_polymorphism/practice_9.cpp
+++ Inheritance_polymorphism/practice_9.cpp
@@ -1,27 +1,40 @@
-/*9. Write a Program of Two 1D Matrix Addition using Operator Overloading.*/
+/*9. Write a Program of Two 1D Matrix Addition using Operator Overloading*/
 
 #include<iostream>
 using namespace std;
-class complex{
+class Matrix{
+	private:
+		int i,e[3];
 	public:
-		int x, y;
-		
-		complex(int a,int b){
-			x =a;
-			y =b;
+		Matrix getMatrix(){
+			cout<<"\n Enter the element of Matrix :";
+			for(i=0;i<3;i++){
+				cin>>e[i];
+			}
 		}
-		
-		complex operator+(complex obj2){
-			complex t(0,0);
-			t.x = x + obj2.x;
-			t.y = y + obj2.y;
-			return t;
+		Matrix operator +(Matrix m){
+			Matrix sumMatrix;
+			for(i=0;i<3;i++){
+				sumMatrix.e[i]=e[i]+m.e[i];
+			}
+			return sumMatrix;
+		}
+		void displayMatrix(){
+			for(i=0;i<3;i++){
+				cout<<e[i];
+			}
 		}
 };
-
 int main(){
-	complex obj1(10,20), obj2(10,20), result(0,0);
-	result = obj1 + obj2;
-	cout<<"\nX:"<<result.x;
-	cout<<"\nY:"<<result.y;
-}//baki he..
+	Matrix m1,m2,m3;
+	
+	cout<<"\n1st matrix";
+	m1.getMatrix();
+	m1.displayMatrix();
+	cout<<"\n2nd Matrix";
+	m2.getMatrix();
+	m2.displayMatrix();
+	m3=m1+m2;
+	cout<<"\nThe sum of two matrices is:";
+	m3.displayMatrix();
+}
